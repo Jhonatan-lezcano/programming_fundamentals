4b177df301b6aff700eb778189df890fa13a1dbe
Jhonatan-lezcano <jhonatanlezcano.plv@gmail.com>
2025-04-10 21:08:15 -0500

    first commit

---
+++ workshop_1_introduction.py
@@ -0,0 +1,65 @@
+import math
+# A = 10 
+# B = 15.5
+# D = 20
+# X = -5
+# Y = 13.0
+# Z = 11.5
+
+# numerador = (A - (X/(X-Y)) + (D/(2+Z)) - X)
+# denominador = B + D
+# r = numerador / denominador
+# r2 = 17//3+12**2-((5+8)*2)
+# a=10
+# b=5
+# c=a
+# a=b
+# b=c
+# c=10
+
+# print(a,b,c)
+
+# print(r)
+# print(r2)
+
+# a = 20
+# c = a
+# b = 10
+# a = b
+# b = c
+# print(b)
+# print(a)
+# b = b + 1
+# print(b)
+
+# r = 20
+# t = r + 6
+# r = r + t
+# print(r)
+# r = r - 2
+
+# a = 10
+# b = 3
+# a = a % b
+# b = 20
+# print(a)
+# a = a + a % b
+# print(a)
+# a = a // 2
+# print(a)
+
+# a = 5.0
+# b = 8.0
+# c = 2.0
+
+# x1 = (-b + math.sqrt(b**2 - 4*a*c)) / (2*a)
+# x2 = (-b - math.sqrt(b**2 - 4*a*c)) / (2*a)
+
+# print(x1, x2)
+
+# r = 9 + 20 / 2**2 - (7 // 3 + 1)
+
+# print(r)
+
+r = 2 + 3 * 5 // 10**2 - 8 * 3
+print(r)
